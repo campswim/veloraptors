f5295f812f661f518d92c68da73d31c1a5421e47
Nathan Cox <nate@campswim.co>
2024-11-22 13:18:35 -0800

    First commit.

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+// Enqueue the parent theme's stylesheet
+function magzine_child_enqueue_styles() {
+    wp_enqueue_style('magzine-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('magzine-child-style', get_stylesheet_uri(), array('magzine-style'));
+}
+add_action('wp_enqueue_scripts', 'magzine_child_enqueue_styles');
