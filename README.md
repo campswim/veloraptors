02eb787090435dfe736d60d53fc3e207d9bc5edb
Nathan Cox <nate@campswim.co>
2024-12-06 13:28:02 -0800

    Style.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# veloraptors
+# Velo Raptors Cycling Club
+
 A WordPress site for the VeloRaptors Cycling Club in the Bay Area
+
+## Theme
+
+Zine by GhostPool.
+
+## Child Theme
+
+Magzine Child by Nathan Cox
--- functions.php
+++ functions.php
@@ -5,3 +5,20 @@ function magzine_child_enqueue_styles() {
     wp_enqueue_style('magzine-child-style', get_stylesheet_uri(), array('magzine-style'));
 }
 add_action('wp_enqueue_scripts', 'magzine_child_enqueue_styles');
+
+// // Enable SVG upload support
+// function enable_svg_upload($mimes) {
+//   $mimes['svg'] = 'image/svg+xml';
+//   return $mimes;
+// }
+// add_filter('upload_mimes', 'enable_svg_upload');
+
+// // Optional: Sanitize SVG files
+// function sanitize_svg($data, $file, $filename, $mimes) {
+//   if ('svg' === $file['type']) {
+//       $data['ext'] = 'svg';
+//       $data['type'] = 'image/svg+xml';
+//   }
+//   return $data;
+// }
+// add_filter('wp_check_filetype_and_ext', 'sanitize_svg', 10, 4);
